29acbcb61619187f49b7e5067ca1de08890117f1
SalamanderCtesiphon <swbrookshire@gmail.com>
2022-12-11 18:49:54 -0600

    completed assignment

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ const ctx = canvas.getContext('2d');
 const width = canvas.width = window.innerWidth;
 const height = canvas.height = window.innerHeight;
 
+const para = document.querySelector('p');
+
 // function to generate random number
 function random(min, max) {
   const num = Math.floor(Math.random() * (max - min + 1)) + min;
@@ -16,15 +18,84 @@ function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
 
-class Ball {
-    constructor(x, y, velX, velY, color, size) {
+class Shape {
+    constructor(x, y, velX, velY, exists) {
         this.x = x;
         this.y = y;
+        this.exists = true;
         this.velX = velX;
         this.velY = velY;
+    }
+}
+
+class EvilCircle extends Shape {
+    constructor(x, y) {
+        super(x, y, 20, 20);
+        this.color = "white";
+        this.size = 30;
+        window.addEventListener("keydown", (e) => {
+            switch (e.key) {
+              case "a":
+                this.x -= this.velX;
+                break;
+              case "d":
+                this.x += this.velX;
+                break;
+              case "w":
+                this.y -= this.velY;
+                break;
+              case "s":
+                this.y += this.velY;
+                break;
+            }
+        });
+    }
+    draw() {
+        ctx.beginPath();
+        ctx.lineWidth = 3;
+        ctx.strokeStyle = this.color;
+        ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+        ctx.stroke();
+    }         
+    checkBounds() {
+        if ((this.x + this.size) >= width) {
+            this.x = width - this.size;
+        }
+
+        if ((this.x - this.size) <= 0) {
+            this.x = this.size;
+        }
+
+        if ((this.y + this.size) >= height) {
+            this.y = height - this.size;
+        }
+
+        if ((this.y - this.size) <= 0) {
+            this.y = this.size;
+        }
+    }
+    collisionDetect() {
+        for (const ball of balls) {
+          if (ball.exists) {
+            const dx = this.x - ball.x;
+            const dy = this.y - ball.y;
+            const distance = Math.sqrt(dx * dx + dy * dy);
+      
+            if (distance < this.size + ball.size) {
+              ball.exists = false;
+            }
+          }
+        }
+      }
+}
+
+class Ball extends Shape {
+    constructor(x, y, velX, velY, color, size) {
+        super(x, y, velX, velY);    
         this.color = color;
         this.size = size;
     }
+
 
     draw() {
         ctx.beginPath();
@@ -56,7 +127,7 @@ class Ball {
 
     collisionDetect() {
         for (const ball of balls) {
-          if (this !== ball) {
+          if (!(this === ball) && ball.exists) {
             const dx = this.x - ball.x;
             const dy = this.y - ball.y;
             const distance = Math.sqrt(dx * dx + dy * dy);
@@ -72,7 +143,7 @@ class Ball {
 
 const balls = [];
 
-while (balls.length < 100) {
+while (balls.length < 25) {
   const size = random(10, 20);
   const ball = new Ball(
     // ball position always drawn at least one ball width
@@ -88,14 +159,32 @@ while (balls.length < 100) {
   balls.push(ball);
 }
 
+const evilCircle = new EvilCircle(random(0, width), random(0, height));
+
+let count = 0;
+
 function loop() {
     ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
     ctx.fillRect(0, 0, width, height);
   
     for (const ball of balls) {
-      ball.draw();
-      ball.update();
-      ball.collisionDetect();
+        if (ball.exists) {
+            ball.draw();
+            ball.update();
+            ball.collisionDetect();
+        }
+    }
+    count = 0;
+    for (const ball of balls) {
+        if (ball.exists) {
+            count++;
+        }
+    }
+    para.textContent = `Ball count: ${count}`;
+    if (count > 0) {
+        evilCircle.draw();
+        evilCircle.checkBounds();
+        evilCircle.collisionDetect();
     }
   
     requestAnimationFrame(loop);
